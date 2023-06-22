afe25b881ff373ff8ca99355662ec8a0ad44cd57
Eliezer <smallcity.programmer@gmail.com>
2023-06-21 22:33:23 -0300

    testando clone

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-/* Randomiza dentro do array 6 digitos que formam uma hexcolor exemplo: #ff86f8 */
+/* Randomiza dentro do array 6 digitos que formam uma hexcolor exemplo: #ff86f8 #fa8335 */
 const btn = document.querySelector(".btn");
 const coloring = document.querySelector(".color")
 
